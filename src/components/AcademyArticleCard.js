ec6296b45aa33ed5c43e4eb3bf05242e89074c69
= <=>
2023-08-11 10:02:33 -0500

    WIP: article landing page; decision on colors pending

--- src/components/AcademyArticleCard.js
+++ src/components/AcademyArticleCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import articleBg from "@site/static/img/Bridgeless.png";
 import ArticleBadge from "./Badges/articleBadge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faClock } from "@fortawesome/free-solid-svg-icons";
 
 export default function AcademyArticleCard(props) {
 
@@ -12,7 +12,7 @@ export default function AcademyArticleCard(props) {
                 <div className="date">
                     <span className="day">5</span>
                     <br />
-                    <span className="month" style={{color: '#6966ff'}}>Feb</span>
+                    <span className="month">Feb</span>
                 </div>
             </div>
             <div style={{marginTop:'5%'}}>
@@ -22,9 +22,11 @@ export default function AcademyArticleCard(props) {
             <div style={{marginTop: '5%'}}>
                 <h3>Market makers vs market takers</h3>
             </div>
-            <div style={{textAlign:'right', marginTop:'25%'}}>
-                <FontAwesomeIcon icon={faClock} />
-                <span style={{marginLeft:5}}>4 Min</span>
+            <div className="row">
+                <div className="col col--12" style={{textAlign:'right', marginTop:'20%'}}>
+                    <FontAwesomeIcon icon={faClock} />
+                    <span style={{marginLeft:5}}>4 Min</span>
+                </div>
             </div>
         </div>
     )
