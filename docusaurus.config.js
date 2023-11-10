3a4fbcb1ed8089fc8d45041c614d9742b49e969c
= <=>
2023-11-10 08:39:06 -0600

    integrated tailwindcss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,6 +111,17 @@ const config = {
         disableInDev: false,
       },
     ],
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
   ],
 
   themeConfig:
